aceecc7f9a9995ef1aa80c1251e69d81d932104b
David Enmanuel Castillo <enmanuelkai@gmail.com>
2025-08-26 21:05:38 -0500

    Subir login form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,75 @@
-import Image from "next/image";
+import Head from "next/head";
 
-export default function Home() {
+export default function Login() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="flex justify-center items-center w-screen h-screen bg-stone-200">
+      <section className="flex flex-row w-280 h-190 p-4 bg-white rounded-3xl">
+        <section className="bg-stone-200 w-105 rounded-3xl p-4">
+          <img
+            src="/images/main.webp"
+            alt="Robot"
+            className="absolute w-180 left-35 bottom-70"
+          />
+        </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <section className="flex flex-col w-1/3 justify-center p-4 ml-40 text-black">
+          <header className="text-gray-400 absolute top-40 right-80">
+            Don&apos;t have an account?{" "}
+            <a href="/signup" className="text-black">
+              Sign up
+            </a>
+          </header>
+
+          <h1 className="text-5xl font-bold mb-7">Sign in</h1>
+          <form>
+            <fieldset className="mb-4">
+              <legend className="text-sm mb-4">
+                Sign in with Open account
+              </legend>
+              <div className="flex justify-between space-x-1.5">
+                <button className="w-1/2 p-2 bg-white border border-gray-200 rounded-2xl flex justify-center items-center">
+                  <img
+                    src="/images/google.svg"
+                    alt="Google"
+                    className="w-5 h-5 mr-2"
+                  />
+                  Google
+                </button>
+                <button className="w-1/2 p-2 bg-white border border-gray-200 rounded-2xl flex justify-center items-center">
+                  <img
+                    src="/images/apple.svg"
+                    alt="Apple"
+                    className="w-5 h-5 mr-2"
+                  />
+                  Apple ID
+                </button>
+              </div>
+            </fieldset>
+            <hr className="border-2 border-stone-100 rounded-2xl mt-6 mb-7"></hr>
+
+            <p className="text-sm mb-2">Or continue with email address</p>
+            <span className="flex flex-row mb-3 w-full p-2 bg-stone-100 rounded-md items-center">
+              <img
+                src="/images/mail.svg"
+                alt="email"
+                className="w-5 h-5 mr-2"
+              />
+              <input type="email" placeholder="tam@ui8.net" />
+            </span>
+            <span className="flex flex-row mb-3 w-full p-2 bg-stone-100 rounded-md items-center ">
+              <img
+                src="/images/password.svg"
+                alt="password"
+                className="w-5 h-5 mr-2"
+              />
+              <input type="password" placeholder="•••••••••••••••••••" />
+            </span>
+            <button className="w-full p-2 bg-blue-600 text-white rounded-2xl">
+              Start trading
+            </button>
+          </form>
+        </section>
+      </section>
+    </main>
   );
 }
